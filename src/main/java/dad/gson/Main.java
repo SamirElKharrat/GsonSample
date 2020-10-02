c0e38af0e78f6c85508acf7ebc9178843194dafb
SamirElKharrat <samirelkharrat@gmail.com>
2020-10-02 14:11:19 +0100

    Comenzamos codigo de GsonSample

---
+++ src/main/java/dad/gson/Main.java
@@ -0,0 +1,31 @@
+package dad.gson;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(Object[] _dato) {
+    Scanner in = new Scanner(System.in);
+
+    
+    System.out.println("Introducir nombre:");
+    String nombre = in.nextLine();
+    
+    System.out.println("Introducir Apellido:");
+    String Apellido = in.nextLine();
+    
+    System.out.println("Introducir Edad:");
+    Integer edad = in.nextInt();
+
+
+	}
+
+	public static void main(String[] args) {
+		
+		Persona p = new Persona();
+
+    
+
+	}
+
+}
---
+++ src/main/java/dad/gson/Persona.java
@@ -0,0 +1,36 @@
+package dad.gson;
+
+
+public class Persona {
+	private String Nombre;
+	private String Apellidos;
+	private Integer Edad;
+	
+
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		this.Nombre=Nombre;
+	}
+	
+	public String getApellidos() {
+		return Apellidos;
+	}
+	
+	public void setApellidos(String Apellidos) {
+		this.Apellidos=Apellidos;
+	}
+	
+	public Integer getEdad() {
+		return Edad;
+	}
+	public void setEdad(Integer edad) {
+		this.Edad=Edad;
+	}
+}
+
+
+
+
