9ceed09885e06ca89b92a284f1e3f121404c0565
SamirElKharrat <samirelkharrat@gmail.com>
2020-10-02 17:24:21 +0100

    Implementamos el scanner y el cambio a json

--- src/main/java/dad/gson/Main.java
+++ src/main/java/dad/gson/Main.java
@@ -2,27 +2,31 @@ package dad.gson;
 
 import java.util.Scanner;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
 	
-	public static void main(Object[] _dato) {
-    Scanner in = new Scanner(System.in);
-
-    
-    System.out.println("Introducir nombre:");
-    String nombre = in.nextLine();
-    
-    System.out.println("Introducir Apellido:");
-    String Apellido = in.nextLine();
-    
-    System.out.println("Introducir Edad:");
-    Integer edad = in.nextInt();
-
-
-	}
-
 	public static void main(String[] args) {
-		
+	    Scanner in = new Scanner(System.in);
 		Persona p = new Persona();
+	    
+	    System.out.println("Introducir nombre:");
+	    p.setNombre(in.nextLine());
+	    
+	    System.out.println("Introducir Apellido:");
+	    p.setApellidos(in.nextLine()); 
+	    
+	    System.out.println("Introducir Edad:");
+	    p.setEdad (in.nextInt());
+	    
+	    in.close();
+	    
+	    System.out.println(p.toString());
+	    
+	    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(p);
+        System.out.println(json);
 
     
 
