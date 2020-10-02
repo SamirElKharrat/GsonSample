3b6349debba02896b4181c2af1ce404be6d5e3eb
SamirElKharrat <samirelkharrat@gmail.com>
2020-10-02 17:24:35 +0100

    Implementamos la clase persona con sus funciones correspondientes

--- src/main/java/dad/gson/Persona.java
+++ src/main/java/dad/gson/Persona.java
@@ -12,7 +12,7 @@ public class Persona {
 		return Nombre;
 	}
 	public void setNombre(String nombre) {
-		this.Nombre=Nombre;
+		this.Nombre = nombre;
 	}
 	
 	public String getApellidos() {
@@ -20,14 +20,19 @@ public class Persona {
 	}
 	
 	public void setApellidos(String Apellidos) {
-		this.Apellidos=Apellidos;
+		this.Apellidos = Apellidos;
 	}
 	
 	public Integer getEdad() {
 		return Edad;
 	}
 	public void setEdad(Integer edad) {
-		this.Edad=Edad;
+		this.Edad = edad;
+	}
+	
+	@Override
+	public String toString() {
+		return " nombre = " + Nombre + "; apellido = " + Apellidos + "; edad = " + Edad;
 	}
 }
 
